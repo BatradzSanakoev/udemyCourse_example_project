d9a5a891a186d80a1aa222be5aa4482c5d4ceabd
Bato <sanakoev.batraz@yandex.ru>
2020-03-02 14:18:00 +0300

    add script.js changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 'use strict'
 
-let obj = {
-    name: "Object",
-    level: 1
+let money = prompt("Ваш бюджет на месяц?", 35000);
+let time = prompt("Введите дату в формате YYYY-MM-DD", 2010-10-22);
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expences : {},
+    optionalExpences : {},
+    income : [],
+    savings : false
 };
 
-let arr = ["one", "two", "three", "four"];
+let expA = prompt("Введите обязательную статью расходов в этом месяце", "Качество");
+let expB = prompt("Во сколько обойдется?", 100000);
+
+appData.expences.expA = expB;
+
+alert(appData.budget/30);
