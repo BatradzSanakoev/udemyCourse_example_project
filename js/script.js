81b6e068bd36f199d8804c5eb57a3c3ad3b7604e
Bato <sanakoev.batraz@yandex.ru>
2020-03-02 14:40:01 +0300

    homework script commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 'use strict'
 
 let money = prompt("Ваш бюджет на месяц?", 35000);
-let time = prompt("Введите дату в формате YYYY-MM-DD", 2010-10-22);
+let time = prompt("Введите дату в формате YYYY-MM-DD", "2010-10-22");
 
 let appData = {
     budget : money,
     timeData : time,
-    expences : {},
+    expenses : {},
     optionalExpences : {},
     income : [],
     savings : false
 };
 
-let expA = prompt("Введите обязательную статью расходов в этом месяце", "Качество");
-let expB = prompt("Во сколько обойдется?", 100000);
+let questFirst = prompt("Введите обязательную статью расходов в этом месяце", "Качество");
+let ansFirst = prompt("Во сколько обойдется?", 100000);
+let questSecond = prompt("Введите обязательную статью расходов в этом месяце", "Скорость");
+let ansSecond = prompt("Во сколько обойдется?", 100000);
 
-appData.expences.expA = expB;
+appData.expenses[questFirst] = ansFirst;
+appData.expenses[questSecond] = ansSecond;
 
 alert(appData.budget/30);
+
+console.log(appData);
